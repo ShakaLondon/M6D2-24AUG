51b57fcf69f7b836fe16145a4269cd762369f766
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-08-26 10:37:09 +0100

    postgres server

---
+++ HW-24AUG/README.md
@@ -0,0 +1,26 @@
+# BLOG API POSTGRES
+
+- Create .env
+
+```
+#-----------app----------------------
+
+PORT=5000
+
+
+#-------------database----------------------
+
+
+PGHOST=localhost
+PGUSER=xxxxxxxxxxxx
+PGDATABASE=xxxxxxxxxxx
+PGPASSWORD=xxxxxxxxxxx
+PGPORT=5432
+
+DATABASE_URL=heroku_db_url
+DATABASE_URL_DEV=postgres://yourusername:yourpassword@localhost:5432/postgres
+
+
+```
+
+- Add your credentials
---
+++ HW-24AUG/src/sql/create-tables.sql
@@ -0,0 +1,32 @@
+
+-- DROP TABLE IF EXISTS public.authors  CASCADE;
+-- DROP TABLE IF EXISTS public.blogs  CASCADE;
+
+CREATE TABLE 
+	IF NOT EXISTS
+		products(
+			product_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			name VARCHAR(255) NOT NULL,
+			description VARCHAR (1000) NOT NULL,
+			brand VARCHAR (50) NOT NULL,
+			image_url TEXT NOT NULL,
+			category VARCHAR (50) NOT NULL,
+			created_at TIMESTAMPTZ DEFAULT NOW(),
+			updated_at TIMESTAMPTZ DEFAULT NOW()
+	);
+
+
+
+	 CREATE TABLE 
+	IF NOT EXISTS
+		blogs(
+			blog_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			title VARCHAR(255) NOT NULL,
+			category VARCHAR (50) NOT NULL,
+			content TEXT NOT NULL,
+			author_id INTEGER REFERENCES authors ON DELETE CASCADE,
+			read_time_unit VARCHAR(10) NOT NULL,
+			read_time_value INTEGER NOT NULL,
+			created_at TIMESTAMPTZ DEFAULT NOW(),
+			updated_at TIMESTAMPTZ DEFAULT NOW()
+	);
